f9380323726b09ebea667eb8bbfa0647d3c357a1
Aruseito <Aruseito@gmail.com>
2021-09-06 11:40:17 +0800

    feat: ✨ 222.[二叉树]完全二叉树的节点个数

---
+++ leetCode/222.[medium]完全二叉树的节点个数.js
@@ -0,0 +1,24 @@
+/*
+ * @lc app=leetcode.cn id=222 lang=javascript
+ *
+ * [222] 完全二叉树的节点个数
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+var countNodes = function (root) {
+  if (root === null) return 0;
+  return 1 + countNodes(root.left) + countNodes(root.right);
+};
+// @lc code=end
