e0a448c7fae475014ed1c95c3f25eed9d8ab4a96
AruSeito <AruSeito@gmail.com>
2021-09-21 16:06:04 +0800

    feat: ✨ 203.[链表]移除链表元素

---
+++ leetCode/203.[easy]移除链表元素.js
@@ -0,0 +1,33 @@
+/*
+ * @lc app=leetcode.cn id=203 lang=javascript
+ *
+ * [203] 移除链表元素
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @param {number} val
+ * @return {ListNode}
+ */
+var removeElements = function (head, val) {
+  const fakeHead = new ListNode(-1, head);
+  let current = fakeHead;
+  while (current.next) {
+    if (current.next.val === val) {
+      current.next = current.next.next;
+    } else {
+      current = current.next;
+    }
+  }
+  return fakeHead.next
+};
+// @lc code=end
+
