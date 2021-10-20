e8f446786a1409ab00d9ac2c9c8c0d9ecd7224ea
AruSeito <AruSeito@gmail.com>
2021-10-20 22:21:06 +0800

    feat: ✨ 94.[二叉树]二叉树的中序遍历（迭代法）

--- leetCode/94.[easy]二叉树的中序遍历.js
+++ leetCode/94.[easy]二叉树的中序遍历.js
@@ -18,14 +18,27 @@
  * @return {number[]}
  */
 var inorderTraversal = function (root) {
-  const res = [];
-  const dg = (root) => {
-    if (root === null) return null;
-    dg(root.left);
-    res.push(root.val);
-    dg(root.right);
+  // const res = [];
+  // const dg = (root) => {
+  //   if (root === null) return null;
+  //   dg(root.left);
+  //   res.push(root.val);
+  //   dg(root.right);
+  // }
+  // dg(root);
+  // return res;
+  const queue = [], res = []
+  let cur = root;
+  while (cur !== null || queue.length) {
+    if (cur !== null) {
+      queue.push(cur);
+      cur = cur.left;
+    } else {
+      cur = queue.pop();
+      res.push(cur.val);
+      cur = cur.right;
+    }
   }
-  dg(root);
   return res;
 };
 // @lc code=end
