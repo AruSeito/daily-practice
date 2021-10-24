ea5e4f0adca6afa77d411130bcda5da46225d8b8
AruSeito <AruSeito@gmail.com>
2021-10-24 21:19:43 +0800

    feat: ✨ 104.[二叉树]二叉树的最大深度（迭代法）

--- leetCode/104.[easy]二叉树的最大深度.js
+++ leetCode/104.[easy]二叉树的最大深度.js
@@ -17,11 +17,28 @@
  * @param {TreeNode} root
  * @return {number}
  */
+// var maxDepth = function (root) {
+//   if (root === null) return 0;
+//   let leftDepth = maxDepth(root.left) + 1;
+//   let rightDepth = maxDepth(root.right) + 1;
+//   return Math.max(leftDepth, rightDepth)
+// };
 var maxDepth = function (root) {
-  if (root === null) return 0;
-  let leftDepth = maxDepth(root.left) + 1;
-  let rightDepth = maxDepth(root.right) + 1;
-  return Math.max(leftDepth, rightDepth)
+  let deepth = 0, queue = [];
+  if (root === null) {
+    return deepth;
+  }
+  queue.push(root);
+  while (queue.length) {
+    deepth++;
+    const length = queue.length
+    for (let i = 0; i < length; i++) {
+      const node = queue.shift();
+      node.left && queue.push(node.left);
+      node.right && queue.push(node.right);
+    }
+  }
+  return deepth;
 };
 // @lc code=end
 
