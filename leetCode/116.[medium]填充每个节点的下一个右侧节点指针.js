0bd5f6466ab01115e7c9c1c0b79a63717854d7f1
AruSeito <AruSeito@gmail.com>
2021-10-24 20:48:47 +0800

    feat: ✨ 116.[二叉树]填充每个节点的下一个右侧节点指针（迭代法）

--- leetCode/116.[medium]填充每个节点的下一个右侧节点指针.js
+++ leetCode/116.[medium]填充每个节点的下一个右侧节点指针.js
@@ -20,16 +20,39 @@
  * @return {Node}
  */
 var connect = function (root) {
-  const mergeTwo = (left, right) => {
-    if (!left || !right) return;
-    left.next = right;
-    mergeTwo(left.left, left.right);
-    mergeTwo(right.left, right.right);
-    mergeTwo(left.right, right.left);
+  // const mergeTwo = (left, right) => {
+  //   if (!left || !right) return;
+  //   left.next = right;
+  //   mergeTwo(left.left, left.right);
+  //   mergeTwo(right.left, right.right);
+  //   mergeTwo(left.right, right.left);
+  // }
+  // if (!root) return null;
+  // mergeTwo(root.left, root.right)
+  // return root;
+  const queue = [], res = root;
+  if (root === null) {
+    return res;
   }
-  if (!root) return null;
-  mergeTwo(root.left, root.right)
-  return root;
+  queue.push(root);
+  while (queue.length) {
+    const length = queue.length;
+    let preNode, curNode
+    for (let i = 0; i < length; i++) {
+      if (i === 0) {
+        preNode = queue.shift();
+        curNode = preNode;
+      } else {
+        curNode = queue.shift();
+        preNode.next = curNode;
+        preNode = curNode;
+      }
+      curNode.left && queue.push(curNode.left);
+      curNode.right && queue.push(curNode.right);
+    }
+    curNode.next = null;
+  }
+  return res;
 };
 // @lc code=end
 
