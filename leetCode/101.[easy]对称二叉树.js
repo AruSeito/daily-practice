53f90c88ffd3c6ed67ca9a388f58ef77f8f451ea
AruSeito <AruSeito@gmail.com>
2021-10-25 23:22:30 +0800

    feat: ✨ 101.[二叉树]对称二叉树（广度优先迭代法）

--- leetCode/101.[easy]对称二叉树.js
+++ leetCode/101.[easy]对称二叉树.js
@@ -17,20 +17,40 @@
  * @param {TreeNode} root
  * @return {boolean}
  */
-const compare = (node1, node2) => {
-  if (node1 === null && node2 !== null) return false;
-  else if (node1 !== null && node2 === null) return false;
-  else if (node1 === null && node2 === null) return true;
-  else if (node1.val !== node2.val) return false;
+// const compare = (node1, node2) => {
+//   if (node1 === null && node2 !== null) return false;
+//   else if (node1 !== null && node2 === null) return false;
+//   else if (node1 === null && node2 === null) return true;
+//   else if (node1.val !== node2.val) return false;
 
-  const isSame1 = compare(node1.left, node2.right);
-  const isSame2 = compare(node1.right, node2.left);
-  return isSame1 && isSame2;
+//   const isSame1 = compare(node1.left, node2.right);
+//   const isSame2 = compare(node1.right, node2.left);
+//   return isSame1 && isSame2;
 
-}
+// }
+// var isSymmetric = function (root) {
+//   if (root === null) return true;
+//   return compare(root.left, root.right);
+// };
+
 var isSymmetric = function (root) {
   if (root === null) return true;
-  return compare(root.left, root.right);
+  const queue = [];
+  queue.push(root.left);
+  queue.push(root.right);
+  while (queue.length) {
+    const left = queue.shift();
+    const right = queue.shift();
+    if (left === null && right === null) continue;
+    if (left === null && right !== null) return false;
+    else if (left !== null && right === null) return false;
+    else if (left.val !== right.val) return false;
+    queue.push(left.left);
+    queue.push(right.right);
+    queue.push(left.right);
+    queue.push(right.left);
+  }
+  return true;
 };
 // @lc code=end
 
