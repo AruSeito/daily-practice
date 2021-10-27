26d5bcc747cad6e44e589de1260d542da022de84
AruSeito <AruSeito@gmail.com>
2021-10-27 23:03:46 +0800

    feat: ✨ 513.[二叉树]找树左下角的值（迭代法）

--- leetCode/513.[medium]找树左下角的值.js
+++ leetCode/513.[medium]找树左下角的值.js
@@ -24,19 +24,34 @@
  */
 var findBottomLeftValue = function (root) {
   // 方法1:
-  let depthest = 0, res = root.val;
+  // let depthest = 0, res = root.val;
 
-  const dg = (root, depth) => {
-    if (root === null) return root;
-    let curDepth = depth + 1;
-    if (curDepth > depthest) {
-      depthest = curDepth;
-      res = root.val;
+  // const dg = (root, depth) => {
+  //   if (root === null) return root;
+  //   let curDepth = depth + 1;
+  //   if (curDepth > depthest) {
+  //     depthest = curDepth;
+  //     res = root.val;
+  //   }
+  //   dg(root.left, curDepth);
+  //   dg(root.right, curDepth);
+  // }
+  // dg(root, 0)
+  // return res;
+  let queue = [], res = 0;
+  if (root === null) return res;
+  queue.push(root);
+  while (queue.length) {
+    const length = queue.length;
+    for (let i = 0; i < length; i++) {
+      const node = queue.shift();
+      if (i === 0) {
+        res = node.val;
+      }
+      node.left && queue.push(node.left);
+      node.right && queue.push(node.right);
     }
-    dg(root.left, curDepth);
-    dg(root.right, curDepth);
   }
-  dg(root, 0)
   return res;
 };
 // @lc code=end
