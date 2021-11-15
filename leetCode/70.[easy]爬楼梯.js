a8e1b465609c981059773118cea59b83f3466b4a
AruSeito <AruSeito@gmail.com>
2021-11-15 21:30:16 +0800

    feat: ✨ 70.[动态规划]爬楼梯

--- leetCode/70.[easy]爬楼梯.js
+++ leetCode/70.[easy]爬楼梯.js
@@ -10,15 +10,24 @@
  * @return {number}
  */
 var climbStairs = function (n) {
-  if (n === 1 || n === 2) return n;
-  let first = 1, second = 2, sum = 0;
-  for (let i = 3; i <= n; i++) {
-    sum = first + second;
-    tmp = second;
-    second = sum;
-    first = tmp;
+  // if (n === 1 || n === 2) return n;
+  // let first = 1, second = 2, sum = 0;
+  // for (let i = 3; i <= n; i++) {
+  //   sum = first + second;
+  //   tmp = second;
+  //   second = sum;
+  //   first = tmp;
+  // }
+  // return sum;
+  const dp = [0, 1, 2];
+  if (n <= 2) return dp[n];
+
+  let i = 3;
+  while (i <= n) {
+    dp[i] = dp[i - 1] + dp[i - 2];
+    i++;
   }
-  return sum;
+  return dp[n];
 };
 // @lc code=end
 
