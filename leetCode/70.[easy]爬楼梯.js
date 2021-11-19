4bc9f2c216fa03126995c1b10875bc2ca224248d
AruSeito <AruSeito@gmail.com>
2021-11-19 23:19:27 +0800

    feat: ✨ 70.[完全背包]爬楼梯

--- leetCode/70.[easy]爬楼梯.js
+++ leetCode/70.[easy]爬楼梯.js
@@ -19,15 +19,24 @@ var climbStairs = function (n) {
   //   first = tmp;
   // }
   // return sum;
-  const dp = [0, 1, 2];
-  if (n <= 2) return dp[n];
+  // const dp = [0, 1, 2];
+  // if (n <= 2) return dp[n];
 
-  let i = 3;
-  while (i <= n) {
-    dp[i] = dp[i - 1] + dp[i - 2];
-    i++;
+  // let i = 3;
+  // while (i <= n) {
+  //   dp[i] = dp[i - 1] + dp[i - 2];
+  //   i++;
+  // }
+  // return dp[n];
+  const dp = new Array(n + 1).fill(0);
+  dp[0] = 1;
+
+  for (let i = 1; i <= n; i++) {
+    for (let j = 1; j <= 2; j++) {
+      if (i >= j) dp[i] += dp[i - j]
+    }
   }
-  return dp[n];
+  return dp[n]
 };
 // @lc code=end
 
