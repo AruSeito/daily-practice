cda67ce767b4ad192df07dd4b66a2995e8daba50
AruSeito <AruSeito@gmail.com>
2021-11-21 17:25:09 +0800

    feat: ✨ 122.[动态规划]买卖股票的最佳时机-ii

--- README.md
+++ README.md
@@ -238,6 +238,7 @@
 - [70.[easy]爬楼梯](leetCode/70.[easy]爬楼梯.js)
 - [96.[medium]不同的二叉搜索树](leetCode/96.[medium]不同的二叉搜索树.js)
 - [121.[easy]买卖股票的最佳时机](leetCode/121.[easy]买卖股票的最佳时机.js)
+- [122.[medium]买卖股票的最佳时机-ii](leetCode/122.[medium]买卖股票的最佳时机-ii.js)
 - [139.[medium]单词拆分](leetCode/139.[medium]单词拆分.js)
 - [198.[medium]打家劫舍](leetCode/198.[medium]打家劫舍.js)
 - [213.[medium]打家劫舍-ii](leetCode/213.[medium]打家劫舍-ii.js)
--- leetCode/122.[medium]买卖股票的最佳时机-ii.js
+++ leetCode/122.[medium]买卖股票的最佳时机-ii.js
@@ -10,11 +10,21 @@
  * @return {number}
  */
 var maxProfit = function (prices) {
-  let sum = 0;
+  // let sum = 0;
+  // for (let i = 1; i < prices.length; i++) {
+  //   sum += Math.max(prices[i] - prices[i - 1], 0);
+  // }
+  // return sum;
+  const dp = new Array(prices.length).fill(0).map(() => new Array(2).fill(0));
+  dp[0][0] -= prices[0];
+
+
   for (let i = 1; i < prices.length; i++) {
-    sum += Math.max(prices[i] - prices[i - 1], 0);
+    dp[i][0] = Math.max(dp[i - 1][0], dp[i - 1][1] - prices[i]);
+    dp[i][1] = Math.max(dp[i - 1][1], dp[i - 1][0] + prices[i])
   }
-  return sum;
+
+  return Math.max(...dp[prices.length - 1])
 };
 // @lc code=end
 
