767d31e0e405130e1fbc84496e91f27b3eadaf0c
AruSeito <AruSeito@gmail.com>
2021-11-24 23:05:51 +0800

    feat: ✨ 42.[单调栈]接雨水.js

--- README.md
+++ README.md
@@ -108,6 +108,7 @@
 
 ### 单调栈
 
+- [42.[hard]接雨水.js](leetCode/42.[hard]接雨水.js)
 - [239.[hard]滑动窗口最大值](leetCode/239.[hard]滑动窗口最大值.js)
 - [316.[medium]去除重复字母](leetCode/316.[medium]去除重复字母.js)
 - [402.[medium]移掉 K 位数字](leetCode/402.[medium]移掉-k-位数字.js)
--- leetCode/42.[hard]接雨水.js
+++ leetCode/42.[hard]接雨水.js
@@ -10,23 +10,48 @@
  * @return {number}
  */
 var trap = function (height) {
-  if (height.length < 2) return 0;
-  const maxLeft = new Array(height.length).fill(0);
-  const maxRight = new Array(height.length).fill(0);
+  // if (height.length < 2) return 0;
+  // const maxLeft = new Array(height.length).fill(0);
+  // const maxRight = new Array(height.length).fill(0);
 
-  maxLeft[0] = height[0];
-  for (let i = 1; i < height.length; i++) {
-    maxLeft[i] = Math.max(height[i], maxLeft[i - 1]);
-  }
+  // maxLeft[0] = height[0];
+  // for (let i = 1; i < height.length; i++) {
+  //   maxLeft[i] = Math.max(height[i], maxLeft[i - 1]);
+  // }
 
-  maxRight[height.length - 1] = height[height.length - 1];
-  for (let i = height.length - 2; i >= 0; i--) {
-    maxRight[i] = Math.max(height[i], maxRight[i + 1]);
-  }
+  // maxRight[height.length - 1] = height[height.length - 1];
+  // for (let i = height.length - 2; i >= 0; i--) {
+  //   maxRight[i] = Math.max(height[i], maxRight[i + 1]);
+  // }
+  // let sum = 0;
+  // for (let i = 0; i < height.length; i++) {
+  //   let count = Math.min(maxLeft[i], maxRight[i]) - height[i];
+  //   if (count > 0) sum += count;
+  // }
+  // return sum;
+  if (height.length <= 2) return 0;
+  const stack = [];
+  stack.push(0);
   let sum = 0;
-  for (let i = 0; i < height.length; i++) {
-    let count = Math.min(maxLeft[i], maxRight[i]) - height[i];
-    if (count > 0) sum += count;
+  for (let i = 1; i < height.length; i++) {
+    let top = stack[stack.length - 1]
+    if (height[i] < height[top]) {
+      stack.push(i);
+    } else if (height[i] === height[top]) {
+      stack.pop();
+      stack.push(i);
+    } else {
+      while (stack.length && height[i] > height[top]) {
+        const mid = stack.pop();
+        top = stack[stack.length - 1];
+        if (stack.length) {
+          const h = Math.min(height[i], height[top]) - height[mid];
+          const w = i - top - 1;
+          sum += h * w;
+        }
+      }
+      stack.push(i);
+    }
   }
   return sum;
 };
