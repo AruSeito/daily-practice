03e2f61f609ca29cb3caa597b3905855a6f01ab3
AruSeito <AruSeito@gmail.com>
2021-12-19 23:47:19 +0800

    feat: ✨ ssr ：服务端的 webpack 配置

---
+++ SSR/build/webpack-server.config.js
@@ -0,0 +1,35 @@
+const path = require("path");
+const nodeExternals = require("webpack-node-externals");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
+module.exports = {
+  entry: {
+    index: path.resolve(__dirname, "../server.js")
+  },
+  mode: "development",
+  target: "node",
+  devtool: "cheap-module-eval-source-map",
+  output: {
+    filename: '[name].js',
+    path: path.resolve(__dirname, "../dist/server")
+  },
+  externals: [nodeExternals()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "../src")
+    },
+    extensions: [".js"]
+  },
+  module: {
+    rules: [{
+      test: /\.js$/,
+      use: "babel-loader",
+      exclude: /node_modules/
+    }]
+  },
+  plugins: [
+    new CopyWebpackPlugin([{
+      from:path.resolve(__dirname,"../public"),
+      to:path.resolve(__dirname,"../dist")
+    }]),
+  ]
+}
